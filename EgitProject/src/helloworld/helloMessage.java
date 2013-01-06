39ebc32eabecd7b2f0febaced5aa3a8d1cf47dff
Suhansanu <suhansanu1@gmail.com>
2013-01-06 13:38:24 +0530

    First Commit! Yay!

---
+++ EgitProject/src/helloworld/helloMessage.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+public class helloMessage {
+
+	public static void main(String args[])
+	{
+		System.out.println("Hello Suhansanu!!! Welcome to the world of github");
+		
+	}
+}
